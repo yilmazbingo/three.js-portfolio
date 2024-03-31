9586366ff3d51eab18c4f189f839e1b9cb54f925
yilmaz <yilmazapis@gmail.com>
2024-03-31 09:53:10 -0400

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={clsx(
           inter.variable,
